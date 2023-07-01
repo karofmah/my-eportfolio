59336b6f2f115eb81130b2c31949895328c3e445
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-07-01 18:13:15 +0200

    Improved responsivity in project section

--- app/components/Project.tsx
+++ app/components/Project.tsx
@@ -5,6 +5,7 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import {GoRepo} from 'react-icons/go';
 import 'hover.css/css/hover-min.css';
 
+
 export default function Project(props:any){
   const [image,setImage]=useState<number>(0)
   const [isHovered, setIsHovered] = useState<boolean>(false);
@@ -12,6 +13,14 @@ export default function Project(props:any){
 
   const images:string[]=props.object.images;
   const icons:any[]=props.object.icons
+
+  const divStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundSize: 'cover',
+    height: '400px'
+  }
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -27,50 +36,41 @@ export default function Project(props:any){
   
     return(
       <div>
-{window.innerWidth>640 &&(
-    <div className="bg-no-repeat bg-cover bg-center h-screen mt-16 duration-500 " style={{backgroundImage: `url(${images[image]})`}}>
+
+    <div className="bg-no-repeat bg-cover bg-center h-11/12 lg:h-screen mt-16 lg:duration-500 " style={innerWidth>1024 ? {backgroundImage: `url(${images[image]})`} : {}}>
  <div
-      className={`flex flex-col rounded-xl bg-white absolute right-0 mr-16 mt-12 bg-gradient-to-br from-blue-900 to-cyan-400 overflow-hidden opacity-95 hover:h-3/5 hover:w-2/5 ${props.height} ${props.width} duration-200 `}
+      className={`flex flex-col lg:rounded-xl lg:absolute lg:right-0 lg:mr-16 lg:mt-12 bg-gradient-to-br from-blue-900 to-cyan-400 lg:overflow-hidden lg:opacity-95 hover:w-2/5  lg:hover:h-3/5 lg:${props.height} lg:${props.width} lg:duration-200 `}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       >  
-      <div className="flex">
+      <div className="flex lg:self-auto self-center">
       <h3 className='m-4 text-xl xl:text-2xl 2xl:text-3xl'> {props.object.title} </h3>
-      {isHovered && 
+      {((window.innerWidth>1024 && isHovered) || (window.innerWidth<1024)) && 
       <a href={props.object.repo}>
         <GoRepo className={`w-7 h-7 mt-5 hvr-float-shadow`}/>
         </a>}
       </div>
+      {window.innerWidth<1024 && 
+       <div className="bg-no-repeat bg-cover bg-center h-64 w-full md:w-3/5 duration-200 self-center" style={{backgroundImage: `url(${images[image]})`} }>
 
-     {!isHovered &&
-     <div className="flex flex-wrap w-fit h-fit">
-      
-      {icons.map((icon) => icon)}
-
+       </div>
+        }
+     
+     {((window.innerWidth>1024 && !isHovered) || (window.innerWidth<1024)) &&
+     <div className="flex flex-wrap w-fit h-fit lg:self-auto self-center">
+      {icons.map((icon,index) => (
+          <div key={index}>
+          {icon}
+          </div>
+          ))}
   </div>
 }
-  {!isHovered && <KeyboardArrowDownIcon className="self-center animate-bounce"/>}
+  {innerWidth>1024 && !isHovered && <KeyboardArrowDownIcon className="self-center animate-bounce"/>}
        <Typography className='m-4 text-xs lg:text-md md:text-sm sm:text-sm xs:text-xs xl:text-lg 2xl:text-xl'>{props.object.description}</Typography>
      </div>
      
     </div>
-    )}
-    {window.innerWidth<640 && (
-      
-      <div className="flex flex-col">
-        <div className="">
-        <img src={props.object.images[image]} alt="Project" />
-
-        </div>
-         <div className="flex mt-8 ">
-         {icons.map((icon) => icon)}
-
-     
-  </div>
-  <Typography className='m-4 text-xs lg:text-md md:text-sm sm:text-sm xs:text-xs xl:text-lg 2xl:text-xl'>{props.object.description}</Typography>
-
-      </div>
-    )}
+    
 
     </div>
 
